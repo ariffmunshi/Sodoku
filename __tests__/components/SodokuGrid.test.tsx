616564ff3c1eec63474880f58a4fd4244295a9bf
Ariff Munshi <munshicorp@gmail.com>
2023-08-20 22:46:48 +0800

    Add test to disable filled grid item

--- __tests__/components/SodokuGrid.test.tsx
+++ __tests__/components/SodokuGrid.test.tsx
@@ -2,11 +2,21 @@ import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import SodokuGrid from '@/components/SodokuGrid';
 
-describe('Sodoku Grid Component layout', () => {
+describe('Sodoku Grid Component', () => {
     it('renders 81 grid items', () => {
         render(<SodokuGrid />);
 
         const gridItems = screen.getAllByTestId('grid-item');
         expect(gridItems.length).toBe(81);
+    });
+
+    it('disables grid items with numbers', () => {
+        render(<SodokuGrid />);
+        const gridItems = screen.getAllByTestId('grid-item');
+        gridItems.forEach((item) => {
+            if (item.value) {
+                expect(item).toBeDisabled();
+            }
+        });
     });
 });
