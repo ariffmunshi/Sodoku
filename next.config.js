52bbbcef89e61ac5ce427a1474410b2074774317
Ariff Munshi <munshicorp@gmail.com>
2023-08-28 22:04:38 +0800

    Reset next config file

--- next.config.js
+++ next.config.js
@@ -1,19 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    async headers() {
-        return [
-            {
-                source: '/api/:path*',
-                headers: [
-                    {
-                        key: 'Cache-Control',
-                        value: 'no-store',
-                    },
-                ],
-            },
-        ];
-    },
-    'Cache-Control': ['no-cache', 'no-store', 'max-age=0', 'must-revalidate'],
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
